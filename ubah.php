41fc56e55bf34e277beef14608bcf120a833c1cb
TiaraKhusnulKhotimah <tiarakhusnulkhotimah017@gmail.com>
2019-05-14 10:34:58 +0700

    hi1

---
+++ ubah.php
@@ -0,0 +1,57 @@
+<?php 
+	require 'koneksi.php';
+	session_start();
+  if (!isset($_SESSION['login'])) {
+    header("location: register.php");
+    exit;  
+  }
+
+	$id = $_GET["id"];
+
+	$ubah = query("SELECT * FROM blog WHERE id = $id")[0];
+
+
+	if (isset($_POST["submit"])) {
+		if (ubah ($_POST) > 0){
+			echo "data berhasil diubah";
+			echo "<br>";
+			echo "klik ";
+			echo "<a href='admin.php'>disini</a>";
+			echo " untuk melihat hasil";
+		}
+		else{
+			echo "data gagal diubah";
+			echo "<br>";
+			echo "klik ";
+			echo "<a href='admin.php'>disini</a>";
+			echo " bray";
+		}
+	}
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Edit articel</title>
+</head>
+<body style="background:url(gmb2.png)"> 
+<body>
+	<br><br><!-- 
+	<?php $waktu = date_default_timezone_set('Asia/Jakarta');  ?> -->
+
+ 	<form method="POST" action="">
+ 		<input type="hidden" name="id" value="<?= $ubah["id"]?>">
+ 		<br>
+ 		Nama Masakan	: 
+		<input type="text" name="namamasakan" placeholder="Judul">
+		<br>
+		<br>
+		Bahan-bahan	:
+		<textarea name="bahan" placeholder="Text Artikel"> </textarea>
+		<br><br>
+		Cara Pembuatan	:
+		<textarea name="langkah" placeholder="Text Artikel"> </textarea>
+		<br><br>
+		<input type="submit" name="submit">
+ 	</form>
+</body>
+</html>
